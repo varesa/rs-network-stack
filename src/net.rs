554baa34504772a8ca9abe4216aeb134575a3104
Esa Varemo <esa@kuivanto.fi>
2020-11-09 23:22:48 +0200

    nesting

--- src/net.rs
+++ src/net.rs
@@ -1,33 +1,26 @@
 use byteorder::{ReadBytesExt, BigEndian};
 use heapless::FnvIndexMap;
 use heapless::consts::*;
-use std::io::BufRead;
 use std::convert::TryInto;
 use std::fmt;
 
-//const OFFSET_DESTINATION_MAC: usize = 0;
-//const OFFSET_SOURCE_MAC: usize = 6;
-const OFFSET_ETHERTYPE: usize = 12;
-const OFFSET_PAYLOAD: usize = 14;
-
 #[derive(Debug)]
-enum EtherType {
-    ARP,
-    IPv4,
-    IPv6,
-    Unknown(u16),
+struct ArpPacket<'a> {
+    payload: &'a [u8],
 }
 
-fn get_ethertype(ethertype: &[u8]) -> EtherType {
-    //println!("{:x?}", &frame[0 .. 17]);
-    //let ethertype = &frame[OFFSET_ETHERTYPE .. OFFSET_PAYLOAD];
-    //println!("{:#?}", fields);
-    match ethertype {
-        [0x08, 0x00] => EtherType::IPv4,
-        [0x08, 0x06] => EtherType::ARP,
-        [0x86, 0xDD] => EtherType::IPv6,
-        _ => EtherType::Unknown(ethertype.clone().read_u16::<BigEndian>().unwrap()),
-    }
+#[derive(Debug)]
+struct UnknownEtherTypePayload<'a> {
+    ethertype: u16,
+    payload: &'a [u8],
+}
+
+#[derive(Debug)]
+enum EtherType<'a> {
+    ARP(ArpPacket<'a>),
+    IPv4,
+    IPv6,
+    Unknown(UnknownEtherTypePayload<'a>),
 }
 
 struct MacAddress<'a> {
@@ -56,27 +49,36 @@ impl fmt::Debug for MacAddress<'_> {
 struct EthernetFrame<'a> {
     source_mac: MacAddress<'a>,
     destination_mac: MacAddress<'a>,
-    ethertype: EtherType,
+    payload: EtherType<'a>,
+}
+
+fn ethertype_slice_to_u16 (ethertype: &[u8]) -> u16 {
+    ethertype.clone().read_u16::<BigEndian>().unwrap()
 }
 
 impl EthernetFrame<'_> {
     fn from_slice(frame: &[u8]) -> EthernetFrame {
-        let (destination_mac, rest) = frame.split_at(6);
-        let (source_mac, rest) = rest.split_at(6);
-        let (ethertype, payload) = rest.split_at(2);
+        let (destination_mac_bytes, rest) = frame.split_at(6);
+        let (source_mac_bytes, rest) = rest.split_at(6);
+        let (ethertype_bytes, payload) = rest.split_at(2);
+
+        let ethertype = ethertype_slice_to_u16(ethertype_bytes);
 
         EthernetFrame {
-            source_mac: MacAddress::from_slice(source_mac),
-            destination_mac: MacAddress::from_slice(destination_mac),
-            ethertype: get_ethertype(ethertype),
+            source_mac: MacAddress::from_slice(source_mac_bytes),
+            destination_mac: MacAddress::from_slice(destination_mac_bytes),
+            payload: match ethertype {
+                0x0800 => EtherType::IPv4,
+                0x0806 => EtherType::ARP(ArpPacket { payload }),
+                0x86DD => EtherType::IPv6,
+                _ => EtherType::Unknown(UnknownEtherTypePayload { ethertype, payload }),
+            },
         }
     }
 }
 
 pub fn update(frame: &[u8]) -> () {
     println!("Received {} bytes", frame.len());
-    //let ethertype = get_ethertype(&frame);
-    //println!("Type: {:x?}", ethertype);
     let frame = EthernetFrame::from_slice(frame);
     println!("{:#x?}", frame);
 }
