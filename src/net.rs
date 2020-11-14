c73a080dcd1012f8290a665ea696c4b733a19ac7
Esa Varemo <esa@kuivanto.fi>
2020-11-14 21:09:56 +0200

    Fix variable case

--- src/net.rs
+++ src/net.rs
@@ -12,15 +12,15 @@ pub fn update<F>(rx_buffer: &mut [u8], tx_buffer: &mut [u8], send: F) -> ()
 where
     F: FnMut(&[u8], usize) -> (),
 {
-    let MY_MAC_BYTES: &mut [u8] = &mut [0x02, 0xDE, 0xAD, 0x00, 0xBE, 0xEF];
-    let MY_IP_BYTES: &mut [u8] = &mut [169, 254, 0, 2];
+    let my_mac_bytes: &mut [u8] = &mut [0x02, 0xDE, 0xAD, 0x00, 0xBE, 0xEF];
+    let my_ip_bytes: &mut [u8] = &mut [169, 254, 0, 2];
 
     println!("Received {} bytes", rx_buffer.len());
     let frame = EthernetFrame::from_slice(rx_buffer);
     println!("{:#x?}", &frame);
     if let EtherType::ARP(arp_packet) = &frame.payload() {
         if ArpOperation::REQUEST == arp_packet.oper() {
-            if &ProtocolAddress::IPv4(MY_IP_BYTES.into()) == arp_packet.tpa() {
+            if &ProtocolAddress::IPv4(my_ip_bytes.into()) == arp_packet.tpa() {
                 println!("Hey, it's us!");
             }
         }
